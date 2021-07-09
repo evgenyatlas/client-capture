be10b849eec3746eef28893e2472438b9ec632d1
Evgeny <gekcy@mail.ru>
2021-07-09 15:24:52 +0300

    я уже и не сосчитаю скок коммитов забыл оставить

--- src/features/geolocation/store.js
+++ src/features/geolocation/store.js
@@ -6,22 +6,10 @@ import { setPayload } from "../../lib/effectorKit/setPayload";
 import { distanceMeters } from "../../lib/geo/distance";
 
 export const setGeolocation = createEvent()
-export const setBbox = createEvent()
 export const $geolocation = createStore({ coords: [0, 0], accuracy: 0 })
 export const $geolocationCoords = $geolocation.map(({ coords }) => coords)
 export const $smothCoords = createStore([0, 0])
-export const $bbox = createStore([0, 0, 0, 0])
 
-$bbox.on(
-    throttle({
-        source: $smothCoords,
-        timeout: 3000
-    }),
-    (state, coords) => {
-
-    }
-)
+$geolocation.on(setGeolocation, setPayload)
 
 $smothCoords.on($geolocation, (currCoords, geo) => distanceMeters(currCoords, geo.coords) >= geo.accuracy ? geo.coords : undefined)
-// export const $smot
-$geolocation.on(setGeolocation, setPayload)
--- src/features/user/user.js
+++ src/features/user/user.js
@@ -1,22 +1,18 @@
 import { attackEv } from "../user/features/attackBtn/store";
 import { $smothCoords } from "../geolocation/store";
 import { captureBuildingUserEv, setUserEv, updateEnergyEv, updateEnergyFactorEv } from "./store";
-import { viewportMapBbox } from "../../lib/mapbox/viewportMapBbox";
-import transformScale from "@turf/transform-scale";
-import booleanContains from "@turf/boolean-contains";
-import { throttle } from "@vkontakte/vkjs";
 
 
 export class User {
     id = ''
     player = null
-    #socket = null
-    #bboxLoaded = null
+    socket = null
+    #loadedBbox = null
     #map
     constructor({ player, socket, map }) {
         this.player = player
         this.#map = map
-        this.#socket = socket
+        this.socket = socket
         this.id = player.id
         //Закидываем нашего игрока в store (по большей части для UI)
         setUserEv({ player: this.player, socket })
@@ -26,47 +22,7 @@ export class User {
         captureBuildingUserEv.watch(this.#captureBuilding)
         //Реакция на события (из UI) атаки
         attackEv.watch(this.#attack)
-        this.onChangeBboxLoaded()
-    }
 
-    onChangeBboxLoaded() {
-        this.#map.addSource('test', {
-            "type": "geojson",
-            "data": null
-        })
-        this.#map.addLayer({
-            id: 'points-of-interest',
-            source: 'test',
-            type: 'line',
-            'paint': {
-                'line-color': 'red',
-                'line-width': 10
-            },
-        })
-        const bbboxLoaded = (bbox) => transformScale(bbox, 1.2)
-        const bbox = viewportMapBbox(this.#map)
-        this.#bboxLoaded = bbboxLoaded(bbox)
-
-        const source = this.#map.getSource('test')
-
-        this.#map.on(
-            'moveend',
-            throttle(async () => {
-                console.time()
-                const bbox = viewportMapBbox(this.#map)
-
-                if (!booleanContains(this.#bboxLoaded, bbox)) {
-                    console.time()
-                    this.#bboxLoaded = bbboxLoaded(bbox)
-
-                }
-                source.setData({
-                    type: 'FeatureCollection',
-                    features: [bbox, this.#bboxLoaded]
-                })
-                console.timeEnd()
-            }, 1000)
-        )
     }
 
     //Метод обновления energy для UI
@@ -76,14 +32,14 @@ export class User {
     //Смена позиции 
     #changePosition = (pos) => {
         this.player.changePosition(pos)
-        this.#socket.emit('changePosition', pos)
+        this.socket.emit('changePosition', pos)
     }
     //Захват строения (вызывается как реакция на события из UI)
     #captureBuilding = (data) => {
         const energyCost = data.building.energyCost
         this.updateEnergy(-energyCost)
         this.player.updateEnergy(-energyCost)
-        this.#socket.emit('captureBuilding', data.building.getDataServer())
+        this.socket.emit('captureBuilding', data.building.getDataServer())
     }
     unCaptureBuilding = captureBuilding => {
         updateEnergyFactorEv(-captureBuilding.energyFactor)
@@ -95,6 +51,6 @@ export class User {
         //Обновляем UI
         this.updateEnergy(-energy)
         //Оповещаем сервер
-        this.#socket.emit('attack', energy)
+        this.socket.emit('attack', energy)
     }
 }
